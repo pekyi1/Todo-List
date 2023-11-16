efd70ace072dadd3da8fe3dd8d64742f65a3330f
pekyi1 <116849498+pekyi1@users.noreply.github.com>
2023-11-16 20:18:45 +0000

    Add files via upload

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -1,71 +1,61 @@
-const todoList = [
-  {name: 'make dinner', dueDate: '2022-12-22'},
-  {name: 'wash dishes', dueDate: '2022-12-22'}
+const todoList = JSON.parse(localStorage.getItem('todos')) || [
+  { name: 'make dinner', dueDate: '2022-12-22' },
+  { name: 'wash dishes', dueDate: '2022-12-22' }
 ];
 renderTodoList();
 
 function renderTodoList() {
   let todoListHTML = '';
 
-todoList.forEach(function (todoObject, index){
-  const {name, dueDate} = todoObject;
-  const html = `
-      <div>${name}</div>
-      <div>${dueDate}</div>
-       <button class="delete-todo-button js-delete-todo-button" >Delete</button>
-      `;
-  todoListHTML += html;
-});
-
-  /*for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
-    // const name = todoObject.name;
-    // const dueDate = todoObject.dueDate;
-    const {name, dueDate} = todoObject;
+  todoList.forEach(function (todoObject, index) {
+    const { name, dueDate } = todoObject;
     const html = `
-        <div>${name}</div>
-        <div>${dueDate}</div>
-         <button class="delete-todo-button" onclick="
-            todoList.splice(${i},1);
-            renderTodoList();
-         ">Delete</button>
-        `;
+    <div class="row gy-3">
+      <div class="col-4 text-center todo-box"><div class="todos">${name}</div></div>
+      <div class="col-4 text-center todo-box"><div class="todos">${dueDate}</div></div>
+      <div class="col-4 delete-button"><button class="delete-todo-button js-delete-todo-button"><span class="text">Delete</span></button></div>
+      </div>
+    `;
     todoListHTML += html;
-  }
-  */
+  });
 
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 
   document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton, index) => {
     deleteButton.addEventListener('click', () => {
-      todoList.splice(index,1);
-          renderTodoList();
+      todoList.splice(index, 1);
+      updateLocalStorage();
+      renderTodoList();
     });
   });
-};
-
-document.querySelector('.js-add-todo-button').addEventListener('click', ()=>{
-  addTodo();
-})
+}
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
   const dateInputElement = document.querySelector('.js-due-date-input');
   const dueDate = dateInputElement.value;
-  todoList.push({
-    // name: name,
-    // dueDate: dueDate
-    name,
-    dueDate
-  });
+  todoList.push({ name, dueDate });
 
-  inputElement.value = '';  // this resets the text in a text box
+  inputElement.value = ''; // this resets the text in a text box
+  updateLocalStorage();
   renderTodoList();
 }
 
-function handleTodoKeydown(event){
-  if(event.key ==='Enter'){
-      addTodo();
+function updateLocalStorage() {
+  localStorage.setItem('todos', JSON.stringify(todoList));
+}
+
+document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+  addTodo();
+});
+
+function handleTodoKeydown(event) {
+  if (event.key === 'Enter') {
+    addTodo();
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('keydown', handleTodoKeydown);
+});
